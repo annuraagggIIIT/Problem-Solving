b895a07874504e7277f7318033e58fdfa7f7ff59
hamna <hamnajabbar36@gmail.com>
2023-10-25 00:25:50 +0500

    length-of-a-string(Hacktoberfest )

---
+++ length_of_a_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello Tech people!Its Hacktoberfest.lets rock together.";
+
+    int length = strlen(str);
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
