8c9a63b6b39aaa88b03f37cb95b271bd0781fe6b
hungb2014918 <89437524+hungb2014918@users.noreply.github.com>
2023-11-08 10:47:53 +0700

    Delete convert_binary_to_decimal/convert_binary_to_decimal_(Python).py

--- convert_binary_to_decimal/convert_binary_to_decimal_(Python).py
+++
@@ -1,6 +0,0 @@
-import math
-
-binarynum = input("Enter the binary number to convert (including 8 numbers): ")
-
-print("The binary number after being converted to decimal is: ", int(binarynum, 2))
-print("--------------------------------")
